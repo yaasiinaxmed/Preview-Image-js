cd0a2f4b906f9e5d8e037064d312a58055b193a0
yaskaboy <109895325+yaasiinaxmed@users.noreply.github.com>
2022-12-21 17:37:24 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const uploadImage = document.querySelector("#uploadImage");
+const imageBox = document.querySelector(".image-box");
+const selectBtn = document.querySelector("#select-btn");
+
+selectBtn.addEventListener("click", () => {
+    uploadImage.click();
+});
+
+uploadImage.addEventListener("change", (e) => {
+   selectBtn.innerHTML = `<div class="loader"></div>
+   Uploading...`;
+
+   setTimeout(() => {
+     if (e.target.files.length == 0) {
+        return;
+     }
+
+     selectBtn.innerText = 'Change Image';
+
+     let image = document.createElement("img");
+     let file = e.target.files[0];
+     let url = URL.createObjectURL(file);
+
+     image.src = url;
+     imageBox.append(image);
+   }, 2000);
+});
